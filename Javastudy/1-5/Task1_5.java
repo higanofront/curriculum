5aa4c261a405a0682ccf8ef39556713844e9c8c7
higanofront <higanofront@yahoo.co.jp>
2021-08-09 18:35:07 +0900

    課題1-5
    制御文（ループ文）

---
+++ Javastudy/1-5/Task1_5.java
@@ -0,0 +1,68 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+ 
+/** 
+ * Task1-5 : 課題内容
+ *
+ * 本課題では、ルーブ文に記述に慣れていきましょう。
+ * 問①〜問④まであります。
+ * for文・while文の仕組みを意識しながらコーディングしていきましょう！
+ */
+public class Task1_5 {
+
+  public static void main(String[] args) {
+
+      // ① 「みかん」、「りんご」、「ぶどう」、「メロン」の値を設定した配列 fruits を作成してください。
+      String[] fruits = {"みかん", "りんご", "ぶどう", "メロン"};
+
+        // ② for文を使って①で作成した配列を出力しなさい。
+      for(String fruit: fruits) {
+        System.out.println(fruit);
+      }
+
+      // ③ 以下のwhile文の処理について、何をしているのかコメントを記入してください。
+      /*
+      *  [iが100以上の場合に{}内を繰り返す,iを出力、iを1インクリメント]
+      */
+      int i = 1;
+      while(i <= 100) {
+        System.out.print(i);
+        i++;
+      }
+      System.out.println();
+
+      /* ④ 行の最初に「段数」と「||」を追加したものを表示させるプログラムを作成しなさい。
+      *     1 || 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+      *     2 || 2 | 4 | 6 | 8 | 10 | 12 | 14 | 16 | 18 |
+      *     3 || 3 | 6 | 9 | 12 | 15 | 18 | 21 | 24 | 27 |
+      *     4 || 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 |
+      *     5 || 5 | 10 | 15 | 20 | 25 | 30 | 35 | 40 | 45 |
+      *     6 || 6 | 12 | 18 | 24 | 30 | 36 | 42 | 48 | 54 |
+      *     7 || 7 | 14 | 21 | 28 | 35 | 42 | 49 | 56 | 63 |
+      *     8 || 8 | 16 | 24 | 32 | 40 | 48 | 56 | 64 | 72 |
+      *     9 || 9 | 18 | 27 | 36 | 45 | 54 | 63 | 72 | 81 |
+      */
+      Map<Integer, List<Integer>> mulutiMaps = new HashMap<>();
+      mulutiMaps.put(1, Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+      mulutiMaps.put(2, Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16, 18));
+      mulutiMaps.put(3, Arrays.asList(3, 6, 9, 12, 15, 18, 21, 24, 27));
+      mulutiMaps.put(4, Arrays.asList(4, 8, 12, 16, 20, 24, 28, 32, 36));
+      mulutiMaps.put(5, Arrays.asList(5, 10, 15, 20, 25, 30, 35, 40, 45));
+      mulutiMaps.put(6, Arrays.asList(6, 12, 18, 24, 30, 36, 42, 48, 54));
+      mulutiMaps.put(7, Arrays.asList(7, 14, 21, 28, 35, 42, 49, 56, 63));
+      mulutiMaps.put(8, Arrays.asList(8, 16, 24, 32, 40, 48, 56, 64, 72));
+      mulutiMaps.put(9, Arrays.asList(9, 18, 27, 36, 45, 54, 63, 72, 81));
+
+      for (Map.Entry<Integer, List<Integer>> mulutiMap : mulutiMaps.entrySet()) {
+        System.out.print(mulutiMap.getKey() + " || ");
+        for(Integer list: mulutiMap.getValue()) {
+          System.out.print(list + " | ");
+        }
+        System.out.println();
+      }
+  }
+}
