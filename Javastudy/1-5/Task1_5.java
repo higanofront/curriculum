19118ae3dab3f41f896c99118314da8097a62393
higanofront <higanofront@yahoo.co.jp>
2021-08-17 09:08:00 +0900

    1-5
    繰り返し処理

--- Javastudy/1-5/Task1_5.java
+++ Javastudy/1-5/Task1_5.java
@@ -20,13 +20,13 @@ public class Task1_5 {
       String[] fruits = {"みかん", "りんご", "ぶどう", "メロン"};
 
         // ② for文を使って①で作成した配列を出力しなさい。
-      for(int i = 0; i < fruits.length; i++) {
-        System.out.println(fruits);
-      }
+        for(String fruit: fruits) {
+          System.out.println(fruit);
+        }
 
       // ③ 以下のwhile文の処理について、何をしているのかコメントを記入してください。
       /*
-      *  [iが100以上の場合に{}内を繰り返す,iを出力、iを1インクリメント]
+      *  [iが100以下の場合に{}内を繰り返す,iを出力、iを1インクリメント]
       */
       int i = 1;
       while(i <= 100) {
@@ -48,10 +48,8 @@ public class Task1_5 {
       */
 
       for (int a = 1; a <= 9; a++) { 
+        System.out.print(a + "||");
         for (int b = 1; b <= 9; b++) { 
-          if(b == 1) {
-            System.out.print(a + "||");
-          }
           System.out.print(a * b + "|");
         }
         System.out.println(); 
