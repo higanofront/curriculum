999052cd14575b6a1b96ff2ce78942ffd799ebdc
higanofront <higanofront@yahoo.co.jp>
2021-08-28 17:20:49 +0900

    2-3
    mainメソッド・インスタンス・メソッドの呼び出し

--- Javastudy/2-2/src/study/User.java
+++ Javastudy/2-2/src/study/User.java
@@ -22,8 +22,8 @@ class User {
     // メソッド名:「printAccountInfo」
 
     protected void printAccountInfo() {
-        System.out.println("ユーザー名は" + this.userName);
-        System.out.println("IDは" + this.id);
+        System.out.println("ユーザー名は、" + this.userName);
+        System.out.println("IDは、" + this.id);
         System.out.println("パスワードは" + this.password);
     }
 }
