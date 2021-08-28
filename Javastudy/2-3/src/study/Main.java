d080b834686dd19b36997fc523eabf2c65e356f8
higanofront <higanofront@yahoo.co.jp>
2021-08-28 17:44:14 +0900

    2-3
    オーバーロード・継承・オーバーライド

--- Javastudy/2-3/src/study/Main.java
+++ Javastudy/2-3/src/study/Main.java
@@ -13,8 +13,8 @@ public class Main {
     public static void main(String[] args) {
 
         // ③ Taskクラスのインスタンスを生成し、「doTask()」メソッドを呼び出しなさい。
-        
-
+        Task task = new Task();
+        task.doTask();
     }
 
 }
